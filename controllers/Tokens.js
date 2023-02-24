a4e9b35954d2abe69c0ba42e2ca1ae17bfd171e8
ketankapale8 <ketan.kapale@gmail.com>
2023-02-25 00:42:02 +0530

    c

--- controllers/Tokens.js
+++ controllers/Tokens.js
@@ -2,9 +2,9 @@ import { User } from "../models/users.js";
 
 export const getAllTokens = async (req , res) =>{
     try{
-        const allUsers = await User.find()
-        const tokens = allUsers.token;
-        res.status(200).json({tokens})
+        const allUsers = await User.find({token})
+        
+        res.status(200).json({allUsers})
 
     }catch(err){
         console.log(err)
