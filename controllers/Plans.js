3f95650da0480ec69ce8ab9efe96301bab11c1fa
ketankapale8 <ketan.kapale@gmail.com>
2023-08-02 18:14:43 +0530

    vercelChanges

--- controllers/Plans.js
+++ controllers/Plans.js
@@ -1,16 +1,16 @@
-import { Plans } from "../models/Plans.js";
-import { User } from "../models/users.js";
+// import { Plans } from "../models/Plans.js";
+// import { User } from "../models/users.js";
 
 
-// add plans //
-export const addPlan = async (req , res) => {
-    const {customerId } = req.body;
+// // add plans //
+// export const addPlan = async (req , res) => {
+//     const {customerId } = req.body;
 
-    const user = await User.findbyId(req.user._id);
-    if(user){
-        user.plan = customerId;
-    }
+//     const user = await User.findbyId(req.user._id);
+//     if(user){
+//         user.plan = customerId;
+//     }
 
 
     
-}
+// }
