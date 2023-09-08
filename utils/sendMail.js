c401a8bf68dfb4f92399cc9caf0ebf9443dfb195
ketankapale8 <ketan.kapale@gmail.com>
2023-09-08 11:08:03 +0530

    changes

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -2,24 +2,20 @@ import {createTransport} from 'nodemailer';
 
 export const sendMail = async (email , subject , text) =>{
     const transport = createTransport({
-        host : process.env.SMTP_HOST,
-        secureConnection : true,
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         secure: false,
-        port : process.env.SMTP_PORT,
         // secure: false,
         logger: true,
   debug: true,
   ignoreTLS: true, // add this ,
         auth : {
-            user : process.env.SMTP_USER,
-            pass : process.env.SMTP_PASS
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
         }, 
         tls : {
             rejectUnauthorized : true
         }
-
-    
-        
     })
     await transport.sendMail({
         from: process.env.SMTP_USER,
