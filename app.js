3b7f8d4eddab01c08d2bc711c6a74d11fe9c6348
ketankapale8 <ketan.kapale@gmail.com>
2023-09-10 19:08:15 +0530

    changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const corsOptions = {
 
 app.use(cors({
     methods : ['GET ,POST ,PUT , PATCH , DELETE '],
-    // origin : "http://localhost:3000",
+    origin : "*",
     credentials : true
 }))
 
