0c98ab5cf184abe9338c2dfbf047b1611d9e7dfc
ketankapale8 <ketan.kapale@gmail.com>
2023-09-10 15:30:43 +0530

    changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@
             }
 
             const decoded = jwt.verify(token , process.env.JWT_SECRET)
-            req.user = await User.findById(decoded._id);
+            req.user = await User.findById(decoded.id);
             console.log(req.user)
             next()
             
