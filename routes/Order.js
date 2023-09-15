93631421701c393efa385bf12dd5599992f3db42
ketankapale8 <ketan.kapale@gmail.com>
2023-09-15 18:29:29 +0530

    changes!

--- routes/Order.js
+++ routes/Order.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { CreateOrder } from '../controllers/orderController';
+import { CreateOrder } from '../controllers/orderController.js';
 const router = express.Router();
 
 router.post('/neworder', CreateOrder);
