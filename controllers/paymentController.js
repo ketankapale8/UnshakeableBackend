c1906b20634898bd49fe77aed84475af48a5c86b
ketankapale8 <ketan.kapale@gmail.com>
2023-09-15 11:38:32 +0530

    changes

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export const ProcessPayment = async (req, res) => {
     const {total, email , user_id , servicePlan , serviceVal , startDate } = req.body;
 
-    const lineItems = [{currency: 'inr', total , email , user_id , servicePlan , serviceVal , startDate}]
+    const lineItems = [{currency: 'inr', total : total * 100 , email , user_id , servicePlan , serviceVal , startDate}]
 
     const session = await stripe.checkout.sessions.create({
         payment_method_types:["card"],
