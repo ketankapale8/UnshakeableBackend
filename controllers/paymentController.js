001576fa534b062625828ab6095d76087b57ab25
ketankapale8 <ketan.kapale@gmail.com>
2023-09-15 11:37:36 +0530

    backendPaymentChange

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -8,9 +8,11 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export const ProcessPayment = async (req, res) => {
     const {total, email , user_id , servicePlan , serviceVal , startDate } = req.body;
 
+    const lineItems = [{currency: 'inr', total , email , user_id , servicePlan , serviceVal , startDate}]
+
     const session = await stripe.checkout.sessions.create({
         payment_method_types:["card"],
-        line_items: [{total , email , user_id , servicePlan , serviceVal , startDate} ],
+        line_items: lineItems,
         mode:"payment",
         success_url:"https://credimotion.netlify.app/sucess",
         cancel_url:"https://credimotion.netlify.app/failure",
