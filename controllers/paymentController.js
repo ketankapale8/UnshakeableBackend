08dafc340ee55433927fd90e5257dcbccdeafb9f
ketankapale8 <ketan.kapale@gmail.com>
2023-09-15 16:34:58 +0530

    changes!!!Amount

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const ProcessPayment = async (req, res) => {
     const myPayment = await stripe.paymentIntents.create({
-        amount : req.body.total,
+        amount : req.body.amount,
         currency: "inr"
     })
 
